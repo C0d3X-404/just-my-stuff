7e129c06e475ed06777d17d9fb2dbfcb12a84531
Simon <simon.schlitzer@gmx.de>
2025-05-23 16:28:54 +0200

    product acces from product page

--- src/pages/products/ItemDetail.jsx
+++ src/pages/products/ItemDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import{Link} from 'react-router-dom'
-function Productview(props) {
+function Productview() {
   const { id } = useParams();
 
   return (
